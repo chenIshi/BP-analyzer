a6b30d50f49c2629fb0f5afde34e3f61bf40fa3b
chenishi <chen.ishi@gmail.com>
2020-04-16 21:51:36 +0800

    Fix library call link and type used

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # BP-analyzer
 Using Intel Pintool to analyze branch prediction
+
+# Quick start guide
+1. Download Pintool from source
+2. Copy bp_analyzer to pin/source/tools/MyPinTool, then modify `makefile.rule` script target
+3. Run `make TARGET=intel64`
+4. Take `tar` as testbench, run with `../../../pin -t obj-intel64/bp_analyzer.so -- /bin/tar`  
+The output log name will be `output.log` in current directory
--- bp_analyzer.cpp
+++ bp_analyzer.cpp
@@ -1,93 +1,151 @@
+#define allocSize 1024
+#define PIN_DEPRECATED_WARNINGS 0
 #include <iostream>
 #include <fstream>
-#include "bp_analyzer.h"
+#include <map>
+#include <list>
+#include <string>
+#include "pin.H"
 
-using std::cerr;
-using std::ofstream;
-using std::ios;
-using std::string;
-using std::endl;
+std::ofstream TraceFile;
+using namespace std;
 
-ofstream OutFile;
-BranchPredictor myBPU;
+KNOB<string> KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool",
+    "o", "output.log", "specify trace file name");
 
-// analysis coroutine
-void ProcessBranch(ADDRINT PC, ADDRINT targetPC, bool BrTaken) {
-	BP_Info pred = myBPU.GetPrediction(PC);
-	if (pred.Taken != BrTaken) {
-		// Direction Mispredicted
-	}
-	if (pred.predTarget != targetPC) {
-		// Target Mispredicted
-		BranchPredictor::setIns(PC, targetPC);
-	}
-	myBPU.Update(PC, BrTaken, targetPC);
-}
+typedef struct {
+    bool Taken;
+    ADDRINT predTarget;
+} BP_Info;
 
-void Instruction(INS ins, void *v) {
-	if (INS_IsBranch(ins) && INS_HasFallThrough(ins)) {
-		INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) ProcessBranch, 
-		IARG_ADDRINT, INS_Address(ins),
-		IARG_ADDRINT, INS_DirectBranchOrCallTargetAddress(ins),
-		IARG_BRANCH_TAKEN, IARG_END);
-	}
-}
+enum BranchStates {
+	STRONGLY_NOT_TAKEN,
+	WEAKLY_NOT_TAKEN,
+	WEAKLY_TAKEN,
+	STRONGLY_TAKEN,
+};
 
-int main() {
-	PIN_Init();
-	INS_AddInstrumentationFunction(Instruction, 0);
-	PIN_StartProgram();
-}
+class BranchPredictor {
+private:
+	BranchStates states [allocSize];
+	ADDRINT currentIns [allocSize];
 
-// BranchPredictor impl
-BranchPredictor::BranchPredictor() {
-	// init state
-	for (int i = 0; i<1024; i++) {
-		states[i] = WEAKLY_TAKEN;
-	}
-}
-
-BranchPredictor::~BranchPredictor() {
-	// dump message
-	double successRate = ((double) bpSuccessCount / (double) (bpFailedCount + bpSuccessCount)) * 100;
-	fprintf(stdout, "Success bp rate: %ld\n", successRate);
-}
-
-BP_Info BranchPredictor::GetPrediction(ADDRINT pc) {
-	int key = getKey(pc);
-	BP_Info ret;
-	ret.Taken = as_bool(states[key]);
-	ret.predTarget = currentIns[key];
-	return ret;
-}
-
-void BranchPredictor::Update(ADDRINT pc, bool brTaken, ADDRINT targetPc) {
-	int key = getKey(pc);
-	// update analysis log
-	// guessed the right branch
-	if (as_bool(states[key]) == brTaken) {
-		bpSuccessCount ++;
-	} else {
-		bpFailedCount ++;
-	}
-	// apply state change
-	switch (states[key]) {
+    void UpdateState(bool BrTaken, ADDRINT PC) {
+		int key = getKey(PC);
+        switch (states[key]) {
 		case STRONGLY_NOT_TAKEN:
-			states[key] = brTaken ? WEAKLY_NOT_TAKEN : STRONGLY_NOT_TAKEN;
+			states[key] = BrTaken ? WEAKLY_NOT_TAKEN : STRONGLY_NOT_TAKEN;
 			break;
 		case WEAKLY_NOT_TAKEN:
-			states[key] = brTaken ? WEAKLY_TAKEN : STRONGLY_NOT_TAKEN;
+			states[key] = BrTaken ? WEAKLY_TAKEN : STRONGLY_NOT_TAKEN;
 			break;
 		case WEAKLY_TAKEN:
-			states[key] = brTaken ? STRONGLY_TAKEN : WEAKLY_NOT_TAKEN;
+			states[key] = BrTaken ? STRONGLY_TAKEN : WEAKLY_NOT_TAKEN;
 			break;
 		case STRONGLY_TAKEN:
-			states[key] = brTaken ? STRONGLY_TAKEN : WEAKLY_TAKEN;
+			states[key] = BrTaken ? STRONGLY_TAKEN : WEAKLY_TAKEN;
 			break;
+		}
+    }
+
+
+public:
+
+	BranchPredictor() {
+		// init state
+		for (int i = 0; i<allocSize; i++) {
+			states[i] = WEAKLY_TAKEN;
+			currentIns[i] = 0;
+		}
 	}
+
+	bool GetState(ADDRINT PC) {
+		switch (states[getKey(PC)]) {
+			case STRONGLY_NOT_TAKEN:
+				return false;
+			case WEAKLY_NOT_TAKEN:
+				return false;
+			case WEAKLY_TAKEN:
+				return true;
+			case STRONGLY_TAKEN:
+				return true;
+		}
+		return true;
+    }
+
+    BP_Info GetPrediction(ADDRINT PC) {
+        BP_Info tmp_info;
+		int key = getKey(PC);
+		if (currentIns[key] == 0) {
+			tmp_info.Taken = true;
+			tmp_info.predTarget = -1;
+		} else {
+			tmp_info.Taken = GetState(PC);
+			tmp_info.predTarget = currentIns[key];
+		}
+        return tmp_info;
+    }
+
+    void Update(ADDRINT PC, bool BrTaken, ADDRINT targetPC) {
+        UpdateState(BrTaken, PC);
+		int key = getKey(PC);
+		currentIns[key] = targetPC;
+    }
+
+	int getKey(ADDRINT PC) {
+		return PC % allocSize;
+	}
+};
+
+BranchPredictor myBPU;
+
+long long int DirectionMissCount = 0;
+long long int TargetMissCount = 0;
+long long int BranchCount = 0;
+
+VOID ProcessBranch(ADDRINT PC, ADDRINT targetPC, bool BrTaken) {
+    
+    BranchCount++;
+    BP_Info pred = myBPU.GetPrediction(PC);
+    if ( pred.Taken != BrTaken) {
+        DirectionMissCount++;
+    }
+    if ( pred.predTarget != targetPC ) {
+        TargetMissCount++;
+    }
+    myBPU.Update(PC, BrTaken, targetPC);
+}
+    
+VOID Instruction(INS ins, VOID *v)
+{
+    if (INS_IsBranch(ins) && INS_HasFallThrough(ins))
+        INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) ProcessBranch, 
+	IARG_ADDRINT, INS_Address(ins), 
+	IARG_ADDRINT, INS_DirectBranchOrCallTargetAddress(ins),
+	IARG_BRANCH_TAKEN, IARG_END);
 }
 
-void BranchPredictor::setIns(ADDRINT pc, ADDRINT targetPc) {
-	int key = getKey(pc);
-	currentIns[key] = targetPc;
+
+VOID Fini(INT32 code, VOID *v)
+{
+    TraceFile << "################################################" << endl;
+    TraceFile << "DirectionMissCount: " << DirectionMissCount << endl;
+    TraceFile << "TargetMissCount: " << TargetMissCount << endl;
+    TraceFile << "BranchCount: " << BranchCount << endl;
+    TraceFile << "Branch Direction Miss rate: " << (DirectionMissCount / (float)BranchCount) * 100 << "%" << endl;
+    TraceFile << "Branch Target Miss rate: " << (TargetMissCount / (float)BranchCount) * 100 << "%" << endl;
+    TraceFile << "################################################" << endl;
+}
+
+int main(int argc, char * argv[])
+{
+    TraceFile.open(KnobOutputFile.Value().c_str());
+    TraceFile.setf(ios::showbase);
+
+    PIN_Init(argc, argv);
+    INS_AddInstrumentFunction(Instruction, 0);
+    PIN_AddFiniFunction(Fini, 0);
+    PIN_StartProgram();
+    
+    return 0;
 }
